626923c9395018b6e063962160afbf6aca34879a
xujie-nm <xujie_nm@163.com>
2014-12-27 20:07:49 +0800

    1227

---
+++ 7Reverse_Integer.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+    public:
+        int reverse(int x) {
+            if(x < 10 && x > -10)
+                return x;
+            int res = 0;
+            vector<int> res_nums;
+            while(x != 0){
+                res_nums.push_back(x%10);
+                x /= 10;
+            }
+            if(res_nums.size() == 10){
+                if(res_nums[0] >2 || res_nums[0] < -2)
+                    return 0;   
+                else if(res_nums[0] == 2 || res_nums[0] == -2){
+                    int temp = 0;
+                    for(vector<int>::iterator it = res_nums.begin() + 1; it != res_nums.end(); ++it){
+                    temp *= 10;
+                    temp += *it;
+                    }
+                    if(temp > 147483647 || temp < -147483648)
+                    return 0;
+                }
+            }
+            for(vector<int>::iterator it = res_nums.begin(); it != res_nums.end(); ++it){
+                res *= 10;
+                res += *it;
+            }
+           return res;
+        }
+};
+
+int main(int argc, const char *argv[])
+{
+    return 0;
+}

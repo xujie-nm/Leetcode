66e4235005186b017db9507d838f56791fe0cd10
xujie_nm <xujie_nm@163.com>
2015-05-17 16:02:22 +0800

    150517

---
+++ 111_Minimum_Depth_of_Binary_Tree.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+int minDepth(TreeNode* root) {
+    if(root == NULL)
+        return 0;
+    if(root->left == NULL)
+        return 1 + minDepth(root->right);
+    else if(root->right == NULL)
+        return 1 + minDepth(root->left);
+    return 1 + min(minDepth(root->right), minDepth(root->left));
+}

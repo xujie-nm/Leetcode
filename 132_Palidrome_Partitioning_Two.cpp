875855c28cb2606cc4e22ae505dbfc62f9f480b3
xujie_nm <xujie_nm@163.com>
2015-06-03 16:55:20 +0800

    150603

---
+++ 132_Palidrome_Partitioning_Two.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int minCut(string s){
+    if(s.empty())
+        return 0;
+    vector<vector<bool> > dp(s.size(), vector<bool>(s.size(), false));
+    vector<int> d(s.size());
+    for (int i = s.size()-1; i >= 0; i--) {
+        d[i] = s.size()-i-1;
+        //初始化为如果是最坏情况，没有超过两个字符的回文
+        //所以需要切的次数为当前长度减１
+        for (int j = i; j < s.size(); j++) {
+            if(s[i] == s[j] && (j-i < 2 || dp[i+1][j-1])){
+                //如果当前前后两个字符相等
+                //那么当当前字符长度小于２或者去掉前后两个字符也是回文的情况
+                //那么当前s[i~j]也是回文
+                dp[i][j] = true;
+                //设置s[i~j]字符串为回文
+                if(j == s.size()-1)
+                    d[i] = 0;
+                //如果s[i~s.size()-1]为回文，那么当前s[i~s.size()-1]最小切的次数为０
+                else if(d[j+1]+1 < d[i])
+                //已经确定s[i~j]是回文，回文切１次
+                //所以s[i~s.size()-1]所需要切的最少的次数是
+                //１＋s[j+1~s.size()-1]所需要切的最少次数
+                //那么如果这个次数小于初始化的最大次数，则进行重新赋值
+                    d[i] = d[j+1]+1;
+            }
+        }
+    }
+    return d[0];
+}
+
+int main(int argc, const char *argv[])
+{
+    cout << minCut("abbab") << endl;
+    return 0;
+}

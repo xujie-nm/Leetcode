15f315eb0196c799b36c70d1f1631226c819fe1c
xujie_nm <xujie_nm@163.com>
2015-06-19 17:39:20 +0800

    150619

---
+++ 146_LRU_Cache.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <list>
+#include <unordered_map>
+using namespace std;
+
+class LRUCache{
+    public:
+        LRUCache(int capacity):size(capacity){};
+
+        int get(int key);
+
+        void set(int key, int value);
+    private:
+        typedef unordered_map<int, pair<list<int>:: iterator, int> > Hash;
+        list<int> lruData;
+        Hash hash;
+        int size;
+};
+
+int LRUCache::get(int key){
+    auto res = hash.find(key);
+    if(res == hash.end())
+        return -1;
+    else{
+        lruData.erase(res->second.first);
+        lruData.push_front(res->first);
+        res->second.first = lruData.begin();
+        return res->second.second;
+    }
+}
+
+void LRUCache::set(int key, int value){
+    auto res = hash.find(key);
+    if(res != hash.end()){
+        lruData.erase(res->second.first);
+        lruData.push_front(res->first);
+        res->second.first = lruData.begin();
+        res->second.second = value;
+        return;
+    }
+
+    if(hash.size() == size){
+        hash.erase(lruData.back());
+        lruData.pop_back();
+    }
+    lruData.push_front(key);
+    hash[key] = make_pair(lruData.begin(), value);
+    return ;
+}

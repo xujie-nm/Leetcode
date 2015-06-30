9b508229402758b5ef61c32d7405adbbe77245ca
xujie_nm <xujie_nm@163.com>
2015-06-30 16:20:00 +0800

    150630

---
+++ 154_Find_Minimum_in_Rotated_Sorted_Array_Two.cpp
@@ -0,0 +1,38 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int findMin(vector<int> &nums){
+    if(nums.size() == 0)
+        return 0;
+    int low = 0;
+    int high = nums.size()-1;
+
+    while(low < high){
+        int mid = (low+high)/2;
+
+        while(nums[mid] == nums[low] && low < mid)
+            low++;
+        while(nums[mid] == nums[high] && high > mid)
+            high--;
+
+        if(nums[mid] > nums[high])
+            low = mid+1;
+        else
+            high = mid;
+    }
+    return nums[low];
+}
+
+int main(int argc, const char *argv[])
+{
+    vector<int> nums;
+    nums.push_back(3);
+    nums.push_back(3);
+    nums.push_back(1);
+    nums.push_back(3);
+    cout << findMin(nums) << endl;
+    return 0;
+}

a775c52fce7288987c6fe3b745a1d5a7999b62cf
xujie-nm <xujie_nm@163.com>
2015-10-21 17:12:29 +0800

    151021

--- 2Median_of_Two_Sorted_Arrays.cpp
+++ 2Median_of_Two_Sorted_Arrays.cpp
@@ -4,56 +4,86 @@
 
 using namespace std;
 
-class Solution {
-    public:
-    double findMedianSortedArrays(int A[], int m, int B[], int n) {
-        vector<int> res_array;
-        double res;
-        int i = 0;
-        int j = 0;
-        while(1)
+double findMedianSortedArrays(int A[], int m, int B[], int n) {
+    vector<int> res_array;
+    double res;
+    int i = 0;
+    int j = 0;
+    while(1)
+    {   
+        if(i < m && j < n)
         {   
-            if(i < m && j < n)
+            if(A[i] < B[j])
             {   
-                if(A[i] < B[j])
-                {   
-                    res_array.push_back(A[i]);
-                    i++;
-                }   
-                else
-                {   
-                    res_array.push_back(B[j]);
-                    j++;
-                }   
-            }   
-            else if(i < m)
-            {   
-                                
                 res_array.push_back(A[i]);
                 i++;
             }   
-            else if(j < n)
+            else
             {   
                 res_array.push_back(B[j]);
                 j++;
             }   
-            else
-                break;
         }   
-        if(res_array.size() % 2 == 1)
+        else if(i < m)
         {   
-            res = res_array[res_array.size()/2];
+                                
+            res_array.push_back(A[i]);
+            i++;
+        }   
+        else if(j < n)
+        {   
+            res_array.push_back(B[j]);
+            j++;
         }   
         else
-        {   
-            res = (double)((res_array[res_array.size()/2] + res_array[res_array.size()/2 - 1]) / (double)2.0);
-        }   
-        return res;
-    
-   }
-};
+            break;
+    }   
+    if(res_array.size() % 2 == 1)
+    {   
+        res = res_array[res_array.size()/2];
+    }   
+    else
+    {   
+        res = (double)((res_array[res_array.size()/2] + res_array[res_array.size()/2 - 1]) / (double)2.0);
+    }   
+    return res;
+}
+
+double findMedianSortedArrays2(vector<int> &nums1, vector<int> &nums2){
+    int n = nums1.size() + nums2.size();
+    int m = n/2;
+    auto it1 = nums1.begin();
+    auto it2 = nums2.begin();
+    int val1 = 0;
+    int val2 = 0;
+    for (int i = 0; i <= m; i++) {
+        val1 = val2;
+        if(it1 < nums1.end() && it2 < nums2.end()){
+            if(*it1 < *it2){
+                val2 = *it1;
+                it1++;
+            }else{
+                val2 = *it2;
+                it2++;
+            }
+        }else if(it1 < nums1.end()){
+            val2 = *it1;
+            it1++;
+        }else if(it2 < nums2.end()){
+            val2 = *it2;
+            it2++;
+        }
+    }
+    if(n%2 == 0){
+        return (double)(val1+val2)/2.0;
+    }else
+        return (double)val2;
+}
 
 int main(int argc, const char *argv[])
 {
+    vector<int> nums1{1,2,3,5,7,9};
+    vector<int> nums2{6,8,10};
+    cout << findMedianSortedArrays2(nums1, nums2) << endl;
     return 0;
 }
--- 4Add_Two_Numbers.cpp
+++ 4Add_Two_Numbers.cpp
@@ -10,55 +10,102 @@ struct ListNode {
     ListNode(int x) : val(x), next(NULL) {}
 };
 
-class Solution {
-    public:
-    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
-        int num1, num2, sum;
-        int flag = 0;
-        ListNode* l3 = new ListNode(-1);
-        ListNode* p = l3;
-        while(l1 || l2)
+ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+    int num1, num2, sum;
+    int flag = 0;
+    ListNode* l3 = new ListNode(-1);
+    ListNode* p = l3;
+    while(l1 || l2)
+    {
+        if(l1)
         {
-            if(l1)
-            {
-                num1 = l1->val;
-                l1 = l1->next;
-            }
-            else
-                num1 = 0;
-            if(l2)
-            {
-                num2 = l2->val;
-                l2 = l2->next;
-            }
-            else
-                num2 = 0;
-            sum = num1 + num2 + flag;
-            if(p->val == -1)
-            {
-                p->val = sum % 10;
-            }
-            else
-            {
-                ListNode* pp = new ListNode(sum % 10);        
-                p->next = pp;
-                p = pp;
-            }
-            flag = sum / 10;
+            num1 = l1->val;
+            l1 = l1->next;
         }
-        if(flag > 0)
+        else
+            num1 = 0;
+        if(l2)
         {
-            ListNode* pp = new ListNode(flag);         
+            num2 = l2->val;
+            l2 = l2->next;
+        }
+        else
+            num2 = 0;
+        sum = num1 + num2 + flag;
+        if(p->val == -1)
+        {
+            p->val = sum % 10;
+        }
+        else
+        {
+            ListNode* pp = new ListNode(sum % 10);        
             p->next = pp;
+            p = pp;
         }
-        return l3;
-    
-    
+        flag = sum / 10;
     }
-};
+    if(flag > 0)
+    {
+        ListNode* pp = new ListNode(flag);         
+        p->next = pp;
+    }
+    return l3;
+}
+
+ListNode* addTwoNumbers2(ListNode* l1, ListNode* l2){
+   int overTen = 0;
+   ListNode *res = l1, *pre = l1;
+   while(l1 != NULL || l2 != NULL){
+        if(l1 != NULL && l2 != NULL){
+            int curVal = l1->val + l2->val + overTen;
+            l1->val = curVal%10;
+            overTen = curVal/10;
+            pre = l1;
+            l1 = l1->next;
+            l2 = l2->next;
+        }else if(l1 != NULL){
+            int curVal = l1->val + overTen;
+            l1->val = curVal%10;
+            overTen = curVal/10;
+            pre = l1;
+            l1 = l1->next;
+        }else if(l2 != NULL){
+            int curVal = l2->val + overTen;
+            l2->val = curVal%10;
+            overTen = curVal/10;
+            pre->next = l2;
+            pre = l2;
+            l2 = l2->next;
+        }
+   }
+   if(overTen > 0){
+        ListNode* node = new ListNode(overTen);
+        pre->next = node;
+   }
+   return res;
+}
 
 
 int main(int argc, const char *argv[])
 {
+    ListNode l11(2);
+    ListNode l12(4);
+    ListNode l13(5);
+    l11.next = &l12;
+    l12.next = &l13;
+    ListNode l21(5);
+    ListNode l22(6);
+    ListNode l23(4);
+    l21.next = &l22;
+    l22.next = &l23;
+
+    ListNode *res;
+    res = addTwoNumbers2(&l11, &l21);
+    while(res != NULL){
+        cout << res->val << " ";
+        res = res->next;
+    }
+    cout << endl;
+
     return 0;
 }

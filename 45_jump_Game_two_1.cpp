6be30a4bd2d056cc73efde2131e06f2826c4748d
xujie-nm <xujie_nm@163.com>
2015-10-25 16:42:43 +0800

    151025

--- 45_jump_Game_two_1.cpp
+++ 45_jump_Game_two_1.cpp
@@ -21,9 +21,36 @@ int jump(int A[], int n){
     }
     return  steps;
 }
+
+// Backtracking TLE
+
+void helper(int &res, vector<int> &nums, int cur, int count){
+    if(cur >= nums.size()-1){
+        if(res > count)
+            res = count;
+        return;
+    }
+
+    for (int i = nums[cur]; i >= 1; i--) {
+        helper(res, nums, cur+i, count+1);
+    }
+}
+
+int jump(vector<int> &nums){
+    if(nums.size() == 1)
+        return 0;
+    int res = INT_MAX;
+    helper(res, nums, 0, 0);
+    return res;
+}
+
 int main(int argc, const char *argv[])
 {
     int A[] ={4, 1, 1, 3, 1, 1, 1};
     cout << jump(A, 7) << endl;
+
+    vector<int> nums{2, 3, 1, 1, 4};
+    cout << jump(nums) << endl;
+
     return 0;
 }

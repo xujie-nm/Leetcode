053dd0c2c166704142393317d5f5c7666bc9dc89
xujie-nm <xujie_nm@163.com>
2015-10-30 17:39:41 +0800

    151030

--- 70_Climbing_Stairs.cpp
+++ 70_Climbing_Stairs.cpp
@@ -20,14 +20,35 @@ int climbStairs(int n){
     return temp[n][0];
 }
 
+// simpier and faster
+int climbStairs2(int n){
+    if(n <= 2)
+        return n;
+    vector<int> temp(n, 0);
+    temp[0] = 1;
+    temp[1] = 2;
+
+    for (int i = 2; i < n; i++) {
+        temp[i] = temp[i-1] + temp[i-2];
+    }
+    return temp[n-1];
+}
+
 int main(int argc, const char *argv[])
 {
     cout << climbStairs(1) << endl;
+    cout << climbStairs2(1) << endl;
     cout << climbStairs(2) << endl;
+    cout << climbStairs2(2) << endl;
     cout << climbStairs(3) << endl;
+    cout << climbStairs2(3) << endl;
     cout << climbStairs(4) << endl;
+    cout << climbStairs2(4) << endl;
     cout << climbStairs(5) << endl;
+    cout << climbStairs2(5) << endl;
     cout << climbStairs(6) << endl;
+    cout << climbStairs2(6) << endl;
     cout << climbStairs(7) << endl;
+    cout << climbStairs2(7) << endl;
     return 0;
 }

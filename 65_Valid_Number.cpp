410dbb28484e528ce53c1e9c24a4ff86e0080bba
xujie-nm <xujie_nm@163.com>
2015-10-30 17:06:58 +0800

    151030

--- 65_Valid_Number.cpp
+++ 65_Valid_Number.cpp
@@ -64,12 +64,83 @@ bool isNumber(string s){
     return true;
 }
 
+// faster
+bool isSpace(char c){
+    return c == ' ';
+}
+
+bool isSgn(char c){
+    return c=='+' || c=='-';
+}
+
+bool isDot(char c){
+    return c == '.';
+}
+
+bool isNum(char c){
+    return c<='9'&&c>='0';
+}
+
+bool isE(char c){
+    return c=='e'||c=='E';
+}
+
+bool isNumber2(string s){
+    int pos = 0;
+    bool haveNum = false;
+
+    // 跳过前面的空格
+    while(pos < s.size() && isSpace(s[pos]))
+        pos++;
+
+    // 接着检查正负号
+    if(pos<s.size() && isSgn(s[pos]))
+        pos++;
+    
+    // 接着检查点之前的数字
+    while(pos<s.size() && isNum(s[pos])){
+        haveNum = true;
+        pos++;
+    }
+
+    // 检查之后是否为点
+    if(pos<s.size() && isDot(s[pos]))
+        pos++;
+
+    // 检查点之后的数字
+    while(pos<s.size() && isNum(s[pos])){
+        haveNum = true;
+        pos++;
+    }
+
+    // 检查是否有e||E
+    if(haveNum && pos<s.size() && isE(s[pos])){
+        haveNum = false;
+        pos++;
+        if(pos<s.size() && isSgn(s[pos]))
+            pos++;
+    }
+
+    // 检查e||E之后的数字
+    while(pos<s.size() && isNum(s[pos])){
+        haveNum = true;
+        pos++;
+    }
+
+    // 跳过剩下的空格
+    while(pos<s.size() && isSpace(s[pos]))
+        pos++;
+
+    // 现在已经检查完，如果字符串还不为空，那么返回false
+    return pos==s.size()&&haveNum;
+}
+
 int main(int argc, const char *argv[])
 {
-    cout << isNumber("0") << endl;
-    cout << isNumber(" 0.1") << endl;
-    cout << isNumber("abc") << endl;
-    cout << isNumber("1 a") << endl;
-    cout << isNumber("2e10") << endl;
+    cout << isNumber2("0") << endl;
+    cout << isNumber2(" 0.1") << endl;
+    cout << isNumber2("abc") << endl;
+    cout << isNumber2("1 a") << endl;
+    cout << isNumber2("2e10") << endl;
     return 0;
 }

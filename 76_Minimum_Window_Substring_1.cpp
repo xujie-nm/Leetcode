99828eba05fe69ad1bc9c6187490e3d966cd30db
xujie-nm <xujie_nm@163.com>
2015-11-03 16:08:04 +0800

    151103

--- 76_Minimum_Window_Substring_1.cpp
+++ 76_Minimum_Window_Substring_1.cpp
@@ -31,7 +31,10 @@ string minWindow(string S, string T){
                 int firCha = (int)S[first];//取出最前面的字符
                 if(count[firCha] > num[firCha])//说明这个字符不是第一个出现的
                     count[firCha]--, first++;//计数器减一，first后移
-                else//说明出现了第一个字符
+                else
+                //count[firCha]==num[firCha]，
+                //说明这个字符在s中出现的次数等于在t中出现的次数    
+                //说明出现了第一个字符
                     break;
             }
             if(minLen > second - first + 1){//如果minLen比这个结果长的话，替换重新区结果

09d3a619fd3179f36cb233131670f367ccce1cf9
xujie-nm <xujie_nm@163.com>
2015-11-10 13:54:46 +0800

    151110

--- 86_Partition_List.cpp
+++ 86_Partition_List.cpp
@@ -18,7 +18,12 @@ ListNode* partition(ListNode*head, int x){
     ListNode* temp11 = NULL;
     ListNode* temp2 = NULL;
     ListNode* temp22 = NULL;
-    
+    // 把一个链表分成两个链表
+    // temp11为链表一的头
+    // temp1为链表一的尾
+    // temp22为链表二的头
+    // temp2为链表二的尾
+    // 最后再把两个链表连接起来
     while(head != NULL){
         if(head->val < x){
             if(temp1 == NULL){
@@ -49,7 +54,48 @@ ListNode* partition(ListNode*head, int x){
     }
     return temp11;
 }
+// faster
+// 但是不会保持相对位置
+ListNode* partition2(ListNode* head, int x){
+    if(head == NULL || head->next == NULL)
+        return head;
+    int countSmall = 0;
+    int size = 0;
+    ListNode* temp1 = head;
+    while(temp1 != NULL){
+        if(temp1->val < x)
+            countSmall++;
+        size++;
+        temp1 = temp1->next;
+    }
+    if(countSmall == 0 || countSmall == size)
+        return head;
 
+    temp1 = head;
+    while(countSmall > 0){
+        temp1 = temp1->next;
+        countSmall--;
+    }
+    ListNode* temp2 = head;
+    ListNode* mid = temp1;
+    while(temp1 != NULL && temp2 != mid){
+        if(temp2->val >= x && temp1->val < x){
+            int temp = temp1->val;
+            temp1->val = temp2->val;
+            temp2->val = temp;
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+        }else if(temp2->val >= x){
+            temp1 = temp1->next;
+        }else if(temp1->val < x){
+            temp2 = temp2->next;
+        }else{
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+        }
+    }
+    return head;
+}
 int main(int argc, const char *argv[])
 {
     ListNode l1(1);
@@ -64,7 +110,7 @@ int main(int argc, const char *argv[])
     l4.next = &l5;
     l5.next = &l6;
 
-    ListNode* res = partition(&l1, 3);
+    ListNode* res = partition2(&l1, 3);
 
     //cout << res->val << " " << res->next->val;
     while(res != NULL){

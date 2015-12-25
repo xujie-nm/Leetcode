c3d92e9e8d64327b30b55020ba63c6187bf7cf11
xujie-nm <xujie_nm@163.com>
2015-12-25 21:32:21 +0800

    151225

--- 231_Power_of_Two.cpp
+++ 231_Power_of_Two.cpp
@@ -13,6 +13,12 @@ bool isPowerOfTwo(int n){//把前面的零都消除，留第一个出现的一
     return n == 1;
 }
 
+bool isPowerOfTwo(int n){
+    if(n <= 0)
+        return false;
+    return (n & (n-1)) == 0;
+}
+
 int main(int argc, const char *argv[])
 {
     cout << isPowerOfTwo(4) << endl;

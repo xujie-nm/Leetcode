6965a7cdb40d1bface7cc7f263c54ce881fdcb9c
xujie-nm <xujie_nm@163.com>
2016-01-05 11:31:53 +0800

    160105

--- 138_CopyListwithRandomPointer.cpp
+++ 138_CopyListwithRandomPointer.cpp
@@ -40,6 +40,67 @@ RandomListNode *copyRandomList(RandomListNode *head){
     return preHead.next;
 }
 
+// use less space
+// 先把节点复制一份
+// 放到每个节点的后面
+void cloneNodes(RandomListNode* head){
+    RandomListNode* node = head;
+    while(node != NULL){
+       RandomListNode* cloned = new RandomListNode(node->label);
+        cloned->next = node->next;
+        cloned->random = NULL;
+
+        node->next = cloned;
+
+        node = cloned->next;
+    }
+}
+
+// 把复制出来的random连接起来
+void connectRandomNodes(RandomListNode* head){
+    RandomListNode* node = head;
+    while(node != NULL){
+        RandomListNode* cloned = node->next;
+        if(node->random != NULL){
+            cloned->random = node->random->next;
+        }
+        node = cloned->next;
+    }
+}
+
+// 把原来的节点和复制出来的节点拆分开来
+RandomListNode* detachList(RandomListNode* head){
+    RandomListNode* node = head; 
+    RandomListNode* clonedHead = NULL;
+    RandomListNode* clonedNode = NULL;
+
+    if(node != NULL){
+        clonedHead = clonedNode = node->next;
+        node->next = clonedNode->next;
+        node = node->next;
+    }
+
+    while(node != NULL){
+        clonedNode->next = node->next;
+        clonedNode = clonedNode->next;
+        node->next = clonedNode->next;
+        node = node->next;
+    }
+
+    return clonedHead;
+}
+
+// 新的方法不使用map来存储原来node与randomNode之间的关系
+// 所以少使用O(randomNodes)个空间
+RandomListNode *copyRandomList2(RandomListNode *head){
+    // 复制节点
+    cloneNodes(head);
+    // 连接random
+    connectRandomNodes(head);
+    // 拆分
+    return detachList(head);
+}
+
 int main(int argc, const char *argv[]){
     
     return 0;

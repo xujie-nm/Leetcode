d9740d9552930ec0a9f7a4bcda0136be391270d4
xujie-nm <xujie_nm@163.com>
2016-01-07 14:27:47 +0800

    160107

--- 283_Move_Zeros.cpp
+++ 283_Move_Zeros.cpp
@@ -1,31 +1,32 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 using namespace std;
 
-//
+// 从尾部开始
 void moveZeros(vector<int> &nums){
     if(nums.size() == 1)
         return;
-    int i = 0;
-    int j = 0;
-    while(j < nums.size() && i < nums.size()){
-        while(nums[i] == 0 && i < nums.size())
-            i++;
-        while(nums[j] != 0 && j < nums.size())
-            j++;
-        if(i > j){
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-            //i++;
-            //j++;
-        }else
-            i++;
+    map<int, int> indexs;
+    int index = 0;
+    for (int i = 0; i < nums.size(); i++) {
+        if(nums[i] != 0){
+            indexs[index] = nums[i];
+            index++;
+        }
+    }
+    //cout << "index: " << index << endl;
+    for (auto it = indexs.begin(); it != indexs.end() ; it++) {
+        nums[it->first] = it->second;
+    }
+
+    for (int i = index; i < nums.size(); i++) {
+        nums[i] = 0;
     }
 }
 
-//
+// 删除，增加
 void moveZeros2(vector<int> &nums){
     int num = 0;
     for (int i = 0; i < nums.size(); i++) {
@@ -39,6 +40,18 @@ void moveZeros2(vector<int> &nums){
     while(num > 0){
         nums.push_back(0);
         num--;
+    }
+}
+
+// swap
+void moveZeros3(vector<int> &nums){
+    // 当前数列中最后一个不为0的数
+    int nonZeros = 0;
+
+    for (int i = 0; i < nums.size(); i++) {
+        if(nums[i] != 0){
+            swap(nums[nonZeros++], nums[i]);
+        }
     }
 }
 
@@ -56,7 +69,7 @@ int main(int argc, const char *argv[])
     nums.push_back(1);
     nums.push_back(0);
 
-    moveZeros2(nums);
+    moveZeros3(nums);
     for (int i = 0; i < nums.size(); i++) {
         cout << nums[i] << " ";
     }

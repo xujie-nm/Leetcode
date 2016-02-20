0d5c8e960a1de60e28ca9a3e2de267d20d91936d
xujie-nm <xujie_nm@163.com>
2016-02-20 17:08:26 +0800

    160220

--- 7Reverse_Integer.cpp
+++ 7Reverse_Integer.cpp
@@ -3,39 +3,54 @@
 
 using namespace std;
 
-class Solution {
-    public:
-        int reverse(int x) {
-            if(x < 10 && x > -10)
-                return x;
-            int res = 0;
-            vector<int> res_nums;
-            while(x != 0){
-                res_nums.push_back(x%10);
-                x /= 10;
+int reverse(int x) {
+    if(x < 10 && x > -10)
+        return x;
+    int res = 0;
+    vector<int> res_nums;
+    while(x != 0){
+        res_nums.push_back(x%10);
+        x /= 10;
+    }
+    if(res_nums.size() == 10){
+        if(res_nums[0] >2 || res_nums[0] < -2)
+            return 0;   
+        else if(res_nums[0] == 2 || res_nums[0] == -2){
+            int temp = 0;
+            for(vector<int>::iterator it = res_nums.begin() + 1; it != res_nums.end(); ++it){
+                temp *= 10;
+                temp += *it;
             }
-            if(res_nums.size() == 10){
-                if(res_nums[0] >2 || res_nums[0] < -2)
-                    return 0;   
-                else if(res_nums[0] == 2 || res_nums[0] == -2){
-                    int temp = 0;
-                    for(vector<int>::iterator it = res_nums.begin() + 1; it != res_nums.end(); ++it){
-                    temp *= 10;
-                    temp += *it;
-                    }
-                    if(temp > 147483647 || temp < -147483648)
-                    return 0;
-                }
-            }
-            for(vector<int>::iterator it = res_nums.begin(); it != res_nums.end(); ++it){
-                res *= 10;
-                res += *it;
-            }
-           return res;
+            if(temp > 147483647 || temp < -147483648)
+            return 0;
         }
-};
+    }
+    for(vector<int>::iterator it = res_nums.begin(); it != res_nums.end(); ++it){
+        res *= 10;
+        res += *it;
+    }
+    return res;
+}
+
+
+// easier way
+int reverse1(int x){
+    int res = 0;
+    while(x){
+        int temp = res*10 + x%10;
+        if(temp/10 != res) // 检查这个数是否越界
+            return 0;
+        res = temp;
+        x /= 10;
+    }
+    return res;
+}
 
 int main(int argc, const char *argv[])
 {
+    cout << reverse(1234) << endl;
+    cout << reverse(-1234) << endl;
+    cout << reverse1(1234) << endl;
+    cout << reverse1(-1234) << endl;
     return 0;
 }

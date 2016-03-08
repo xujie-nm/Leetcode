0ad09ec58acd3f9313d36aa4255c8d34ecb25533
xujie-nm <xujie_nm@163.com>
2016-03-08 19:34:37 +0800

    160308

--- 10Regular_Expression_Matching.cpp
+++ 10Regular_Expression_Matching.cpp
@@ -18,8 +18,30 @@ class Solution {
    }
 };
 
+// More effective recursion
+bool helper(string& s, int i, string& p, int j, vector<vector<int> > &memory){
+    if(memory[i][j] != -1)
+        return memory[i][j];
+
+    if(j == p.length())
+        memory[i][j] = (i == s.length());
+    else if(j + 1 < p.length() && p[j + 1] == '*')
+        memory[i][j] = helper(s, i, p, j + 2, memory) || i < s.length() &&
+                       (s[i] == p[j] || p[j] == '.') && helper(s, i+1, p, j, memory);
+    else
+        memory[i][j] = i < s.length() && (s[i] == p[j] || p[j] == '.') && 
+                       helper(s, i+1, p, j+1, memory);
+
+    return memory[i][j];
+}
+
+bool isMatch(string s, string p){
+    vector<vector<int> > memory(s.length()+1, vector<int>(p.length()+1, -1));
+    return helper(s, 0, p, 0, memory);
+}
+
 int main(int argc, const char *argv[])
 {
-    
+    cout << isMatch("aab", "c*a*b") << endl;
     return 0;
 }

da54225314a5edee179c859aada224480cdaed05
xujie-nm <xujie_nm@163.com>
2016-11-19 14:51:39 +0800

    161119

--- 377_Combination_Sum_4.cpp
+++ 377_Combination_Sum_4.cpp
@@ -23,6 +23,7 @@ void helper(vector<int>&nums, int target, int cur, int&res)
 	}
 }
 
+
 int combinationSum4(vector<int>& nums, int target)
 {
 	int res = 0;
@@ -32,9 +33,27 @@ int combinationSum4(vector<int>& nums, int target)
 	return res;
 }
 
+int combinationSum4_2(vector<int>& nums, int target)
+{
+	vector<int> dp(target+1, 0);
+	dp[0] = 1;
+	for (int i = 1; i <= target; ++i)
+	{
+		for (int j = 0; j < nums.size(); ++j)
+		{
+			if(i >= nums[j])
+			{
+				dp[i] += dp[i-nums[j]];
+			}
+		}
+	}
+	return dp[target];
+}
+
 int main(int argc, char const *argv[])
 {
 	vector<int> nums{1,2,3};
 	cout << "result: " << combinationSum4(nums, 4) << endl;
+	cout << "result: " << combinationSum4_2(nums, 4) << endl;
 	return 0;
 }
